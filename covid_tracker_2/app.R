5c47b1106dc3d6466580d6a803b08fb4a120cd37
Ihar Valovich <iv.volovich@gmail.com>
2020-05-23 11:28:19 +0200

    add app.r

---
+++ covid_tracker_2/app.R
@@ -0,0 +1,111 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(utils)
+library(httr)
+
+library(ggplot2)
+library(plotly)
+library(colourpicker)
+library(readr)
+library(lubridate)
+library(dplyr)
+
+#download the dataset from the ECDC website to a local temporary file
+#GET("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", authenticate(":", ":", type="ntlm"), write_memory())
+#write_disk(tf <- tempfile(fileext = ".csv")))
+
+#read the Dataset sheet into “R”. The dataset will be called "data".
+data <- read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", stringsAsFactors = TRUE)
+
+data$dateRep   <- as.character(data$dateRep)   
+data <- data %>%
+    mutate(days = dmy(data$dateRep))
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    titlePanel("The number of people to have died with the coronavirus"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            #textInput("title", "Title", "GDP vs life exp"),
+            selectInput("continents", "Country",
+                        choices = levels(data$countriesAndTerritories),
+                        multiple = FALSE,
+                        selected = "Italy"),
+            numericInput("size", "Point size", 1, 1),
+            checkboxInput("fit", "Add line of best fit", FALSE),
+            colourInput("color", "Point color", value = "blue"),
+            #out = h5("This is a static text"),
+            
+            
+            # sliderInput("years", "Years",
+            # min(gapminder$year), max(gapminder$year),
+            # value = c(1977, 2002))
+            
+        ),
+        mainPanel(
+            # Replace the `plotOutput()` with the plotly version
+            plotlyOutput("plot"),
+            plotlyOutput("plot_cases"),
+            h4("The downloadable data file from European Centre for Disease Prevention and Control is updated daily and contains the latest available public data on COVID-19")
+        )
+    )
+)
+
+# Define the server logic
+server <- function(input, output) {
+    # Replace the `renderPlot()` with the plotly version
+    
+    # Replace the `renderPlot()` with the plotly version
+    output$plot_cases <- renderPlotly({
+        # Convert the existing ggplot2 to a plotly plot  -new cases
+        ggplotly({
+            data <- subset(data,
+                           countriesAndTerritories %in% input$continents) # &
+            #year >= input$years[1] & year <= input$years[2])
+            
+            p_cases <- ggplot(data, aes(days, cases)) +
+                
+                geom_point(size = input$size, col = input$color, alpha = 0.7) + geom_smooth()
+            #scale_x_log10() +
+            
+            ggtitle(input$continents)
+            
+            if (input$fit) {
+                p_cases <- p_cases + geom_smooth(method = "lm")
+            }
+            p_cases
+        })
+    })
+    
+    output$plot <- renderPlotly({
+        # Convert the existing ggplot2 to a plotly plot
+        ggplotly({
+            data <- subset(data,
+                           countriesAndTerritories %in% input$continents) # &
+            #year >= input$years[1] & year <= input$years[2])
+            
+            p <- ggplot(data, aes(days, deaths)) +
+                
+                geom_point(size = input$size, col = input$color, alpha = 0.7) + geom_smooth() +
+                #scale_x_log10() +
+                
+                ggtitle(input$continents)
+            
+            if (input$fit) {
+                p <- p + geom_smooth(method = "lm")
+            }
+            p
+        })
+    })
+}
+
+shinyApp(ui = ui, server = server)
